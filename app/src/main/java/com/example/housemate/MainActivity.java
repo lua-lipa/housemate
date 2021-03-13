9aebd6dd4401869c4d34c77d91dee7a6ac6103b4
eronaaliu <50829986+eronaaliu@users.noreply.github.com>
2021-03-13 23:52:14 +0000

    sign up and login pages design

--- app/src/main/java/com/example/housemate/MainActivity.java
+++ app/src/main/java/com/example/housemate/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.housemate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText fullName, email, password;
+    Button BtnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
