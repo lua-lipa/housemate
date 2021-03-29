4067864a084a45abef6bd157d4762de419df467a
johnnyaokay <49286386+johnnyaokay@users.noreply.github.com>
2021-03-29 16:37:27 +0100

    Added Home Activity Nav and setup fragments

---
+++ app/src/main/java/com/example/housemate/BillsFragment.java
@@ -0,0 +1,28 @@
+package com.example.housemate;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BillsFragment extends Fragment {
+
+    public BillsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bills, container, false);
+    }
+}
