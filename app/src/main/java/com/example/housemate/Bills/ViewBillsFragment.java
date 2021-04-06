81eda248fba2c6c65bdaa772ce3ac050ac49440f
nutsachichilidze <36604389+nutsachichilidze@users.noreply.github.com>
2021-04-06 15:03:11 +0100

    changed bills UI, added swipe to dismiss functionality to the recyclerview

--- app/src/main/java/com/example/housemate/Bills/ViewBillsFragment.java
+++ app/src/main/java/com/example/housemate/Bills/ViewBillsFragment.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -80,12 +81,27 @@ public class  ViewBillsFragment extends Fragment {
         recyclerView = v.findViewById(R.id.bills_recycler_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(activity)); /* activity meant to be .this */
-
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(recyclerView);
 
 
         return v;
 
     }
+
+    ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            /* remove the card object from the view */
+            billsList.remove(viewHolder.getAdapterPosition());
+            billRecyclerViewAdapter.notifyDataSetChanged();
+
+        }
+    };
 
     @Override
     public void onStart() {
